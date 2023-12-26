447ccb16eaae244e61264871e6340f02f8c08cb4
d-el <d2.718l@gmail.com>
2023-12-26 23:46:34 +0200

    Update Firmware and script
    Signed-off-by: d-el <d2.718l@gmail.com>

--- Firmware/Scripts/iv-curve.py
+++ Firmware/Scripts/iv-curve.py
@@ -1,31 +1,28 @@
 #!/usr/bin/env python3
 # Measure Current–voltage characteristic
-# ./iv-curve -i 192.168.0.10 -n 1n4148
 
 from ps3604l import Ps3604l
 import argparse
-import datetime
-from collections import OrderedDict
 import time
 import csv
-import os
 import subprocess
 
+# Settings
 Rw = 0.022 # [Ohm] Wires resistance
 Vlimit = 6 # [V]
 Istart = 0.01 # [A]
-Iend = 0.4 # [A]
+Iend = 1.2 # [A]
 
 Ithreshold = 0.15 # [A]
 
-Istep1 = 0.005 # [A]
+Istep1 = 0.001 # [A]
 Time1 = 0.5 # [s]
 
 Istep2 = 0.02 # [A]
 Time2 = 0.3 # [s]
 
 # SetUp
-ap = argparse.ArgumentParser()
+ap = argparse.ArgumentParser(description='I-V measure')
 ap.add_argument("-i", "--ipaddr", required=False, help="IP address PS3604L", default="192.168.1.10")
 ap.add_argument("-n", "--name", required=True, help="Name")
 ap.add_argument("-d", "--description", required=False, help="Description")
@@ -35,6 +32,7 @@ ps = Ps3604l(args.ipaddr)
 ps.target_voltage = Vlimit
 ps.target_current = 0
 ps.target_mode = ps.Mode.limitation
+ps.target_time = 0
 ps.target_enable = 1
 time.sleep(0.1)
 
@@ -56,18 +54,18 @@ while True:
 			else:
 				current = current + Istep2
 				time.sleep(Time2)
-			im = ps.state_voltage
-			um = ps.state_current #- im * Rw
-			print("{u}V\t{im}A, (set {i}A)".format(u=um, im=im, i=current))
+			um = ps.state_voltage #- im * Rw
+			im = ps.state_current
+			print("{:6.3f}V\t{:5.3f}A, (set {:5.3f}A)".format(um, im, current))
 			um = um
 			writer.writerow([um, im])
 
 		f.close()
 
 		# Set 0A, 0V, disable
+		ps.target_enable = 0
 		ps.target_voltage = 0
 		ps.target_current = 0
-		ps.target_enable = 0
 
 		# Create Graph
 		proc = subprocess.Popen(['gnuplot','--persist'], 
@@ -88,5 +86,7 @@ while True:
 		
 	except KeyboardInterrupt:
 		ps.target_enable = 0
+		ps.target_voltage = 0
+		ps.target_current = 0
 		break
 
--- Firmware/Scripts/ps3604l.py
+++ Firmware/Scripts/ps3604l.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# ./ps3604l -i 192.168.0.10
 
 from pymodbus import (
     ExceptionResponse,
@@ -19,19 +18,27 @@ class Ps3604l():
 	slaveaddr = 1
 
 	def __init__(self, ip, port=502):
-		self.client = client.ModbusTcpClient(ip, port=port)
-		print('connection to {}:{}'.format(ip, port))
+		self.client = client.ModbusTcpClient(ip, port=port,
+			timeout=3,
+			retries=3,
+			retry_on_empty=True,
+			reconnect_delay=1,
+			reconnect_delay_max=9)
+		print('Connection to {}:{}'.format(ip, port), end=' ')
 		r = self.client.read_holding_registers(0x0000, 3, slave=self.slaveaddr)
 		print('Regulator version {major}.{minor}.{patch}'.format(major=r.registers[0], minor=r.registers[1], patch=r.registers[2]))
+	
+	def __del__(self):
+		self.client.close()
 	
 	class Mode(enum.Enum):
 		overcurrentShutdown = 0
 		limitation = 1
-		timeShutdown = 2
 		lowCurrentShutdown = 3
 		dacMode = 4
 	
 	class Status(enum.Flag):
+		normal = 0
 		errorExternalIAdc = 1
 		errorTemperatureSensor = 2
 		overheated = 4
@@ -40,6 +47,17 @@ class Ps3604l():
 		notCalibrated = 32
 		limitation = 64
 		externaIAdc = 128
+	
+	class Disablecause(enum.Enum):
+		none = 0
+		errorTemperatureSensor = 1
+		overheated = 2
+		lowInputVoltage = 3
+		reverseVoltage = 4
+		overCurrent = 5
+		timeShutdown = 6
+		lowCurrentShutdown = 7
+		request = 8
 	
 	def __write_u16(self, reg, val):
 		self.client.write_register(reg, val)
@@ -67,7 +85,7 @@ class Ps3604l():
 	target_voltage = property(lambda self: self.__read_u32(0x0100)/1000000.0, lambda self, val: self.__write_u32(0x0100, int(float(val)*1000000)))
 	target_current = property(lambda self: self.__read_u32(0x0102), lambda self, val: self.__write_u32(0x0102, int(float(val)*1000000)))
 	target_mode = property(lambda self: self.Mode(self.__read_u16(0x0106)), lambda self, val: self.__write_u16(0x0106, val.value))
-	target_time = property(lambda self: self.__read_u32(0x0107), lambda self, val: self.__write_u32(0x0107, int(val)))
+	target_time = property(lambda self: self.__read_u32(0x0107)/1000.0, lambda self, val: self.__write_u32(0x0107, int(val*1000.0)))
 	target_enable = property(lambda self: self.__read_u16(0x0109), lambda self, val: self.__write_u16(0x0109, val))
 	
 	state_voltage = property(lambda self: self.__read_u32(0x0200)/1000000.0, None)
@@ -76,35 +94,75 @@ class Ps3604l():
 	state_input_voltage = property(lambda self: self.__read_u32(0x020C)/1000000.0, None)
 	state_temperature = property(lambda self: self.__read_i16(0x020E)/10.0, None)
 	state_status = property(lambda self: self.Status(self.__read_u16(0x020F)), None)
-
+	state_disablecause = property(lambda self: self.Disablecause(self.__read_u16(0x0210)), None)
+	
+	class State():
+		pass
+	
+	def getState(self):
+		result = self.client.read_holding_registers(0x0200, 21,  slave=self.slaveaddr)
+		decoder = payload.BinaryPayloadDecoder.fromRegisters(result.registers, byteorder=constants.Endian.BIG, wordorder=constants.Endian.LITTLE)
+		s = self.State()
+		s.voltage = decoder.decode_32bit_uint()/1000000.0
+		s.current = decoder.decode_32bit_uint()/1000000.0
+		s.power = decoder.decode_32bit_uint()/1000000.0
+		s.resistance = decoder.decode_32bit_uint()/1000.0
+		s.time = decoder.decode_32bit_uint()/1000.0
+		s.capacity = decoder.decode_32bit_uint()/1000.0
+		s.input_voltage = decoder.decode_32bit_uint()/1000000.0
+		s.temperature = decoder.decode_16bit_int()/10.0
+		s.status = self.Status(decoder.decode_16bit_uint())
+		s.disablecause = self.Disablecause(decoder.decode_16bit_uint())
+		s.vadc = decoder.decode_16bit_uint()
+		s.iadc = decoder.decode_16bit_uint()
+		s.iexternaladc = decoder.decode_32bit_uint()
+		return s
+	
 
 if __name__ == '__main__':
-	ap = argparse.ArgumentParser()
+	ap = argparse.ArgumentParser(description='API PS3604L')
 	ap.add_argument('-i', '--ipaddr', required=True, help='Device IP address')
-	ap.add_argument('-v', '--voltage', required=True, help='voltag', type=float)
-	ap.add_argument('-c', '--current', required=True, help='current', type=float)
-	ap.add_argument('-t', '--time', help='time')
+	ap.add_argument('-v', '--voltage', type=float, required=True, help='voltag')
+	ap.add_argument('-c', '--current', type=float, required=True, help='current')
+	ap.add_argument('-t', '--time', type=float, help='time')
 	ap.add_argument('-w', help='Wait', action='store_true')
 	args = ap.parse_args()
 	
 	ps = Ps3604l(args.ipaddr)
 	ps.target_voltage = args.voltage
 	ps.target_current = args.current
+	ps.target_mode = ps.Mode.limitation
 	if args.time:
 		ps.target_time = args.time
-		ps.target_mode = ps.Mode.timeShutdown
-	else:
-		ps.target_mode = ps.Mode.limitation
 	ps.target_enable = 1
 
 	while True:
 		try:
-			clear = '\x1b[2K'
-			print(clear, 'voltage: {} | current: {} | time: {} | mode: {} | enable: {} | temperature {} | {}'.format(
-				ps.state_voltage, ps.state_current, ps.state_time, ps.target_mode, ps.target_enable, ps.state_temperature, ps.state_status), end='\r')
+			s = ps.getState()
+			print('| voltage: {:6.3f}V | current: {:5.3f}A | power: {:6.3f}W | resistance: {:8.2f}Ω | capacity: {:12.3f}Ah | {:35} |'.format(
+				s.voltage,
+				s.current,
+				s.power,
+				s.resistance,
+				s.capacity,
+				s.disablecause)
+				+ ' '*20)
+			
+			lineUp='\x1B[F'
+			print('| enable: {:8} | input: {:7.1f}V | t: {:8.1f} °C | time: {:14.3f}s | {:24} | {:35} |'.format(
+				ps.target_enable,
+				s.input_voltage,
+				s.temperature,
+				s.time,
+				ps.target_mode,
+				s.status)
+				+ ' '*20, end=lineUp)
+
 			time.sleep(0.1)
 		
 		except KeyboardInterrupt:
 			ps.target_enable = 0
-			print(clear)
+			ps.target_voltage = 0
+			ps.target_current = 0
+			print('\n\n')
 			break
